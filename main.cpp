e7a85b2dcd2fc07b6b4f9c412bf513522b257b75
Marcin Sławicz <marcin.slawicz@gmail.com>
2023-03-25 11:53:41 +0100

    blinking LED test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "mbed.h"
 
+DigitalOut systemLed(LED2);
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
-
+    while (true)
+    {
+        systemLed.write(1);
+        ThisThread::sleep_for(500);
+        systemLed.write(0);
+        ThisThread::sleep_for(500);
     }
 }
 
